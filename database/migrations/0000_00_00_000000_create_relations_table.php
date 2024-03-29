9e22e180834d6196655c0d13545d7e017ec40906
Railken <railken@protonmail.ch>
2024-03-29 16:33:54 +0100

    update migration

--- database/migrations/0000_00_00_000000_create_relations_table.php
+++ database/migrations/0000_00_00_000000_create_relations_table.php
@@ -13,7 +13,7 @@ class CreateRelationsTable extends Migration
     public function up()
     {
         Schema::create(Config::get('amethyst.relation.data.relation.table'), function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('key')->nullable();
             $table->string('source_type');
             $table->integer('source_id')->unsigned();
